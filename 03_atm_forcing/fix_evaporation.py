ded7da04ca6b5010191795cc850981989d0105c8
Anfisa Berezina <fisa4247@gmail.com>
2025-04-01 21:55:29 +0200

    add docstring

--- 03_atm_forcing/fix_evaporation.py
+++ 03_atm_forcing/fix_evaporation.py
@@ -2,8 +2,28 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 
-ds = xr.open_dataset('forcing/adamselv_forcing_2013.nc', decode_times=False)
-# ds = xr.open_dataset('output/adamselv_forcing_TEST.nc', decode_times=False)
+"""
+This script modifies an existing NetCDF file by setting all evaporation values to zero and adjusting the time values.
+
+Steps:
+1. Open the NetCDF dataset using xarray.
+2. Set all values of the 'evap' variable to zero.
+3. Adjust the 'time' variable by subtracting 1826 days (5 year from 2013 to 2018).
+4. Save the modified dataset to a new NetCDF file.
+
+Example:
+python fix_evaporation.py
+
+Dependencies:
+- xarray
+- numpy
+- matplotlib
+"""
+
+ds = xr.open_dataset('output/adamselv_forcing_201802-06.nc', decode_times=False)
 shape = ds['evap'].shape
-ds['evap'].values = np.zeros(shape) # set all values to zero
-ds.to_netcdf('forcing/adamselv_forcing_evap0.nc')
+ds['evap'].values = np.zeros(shape)  # set all values to zero
+
+ds = ds.assign_coords(time=ds['time'].values - 1826)  # subtract 5 years
+ds['Itime'].values = ds['Itime'].values - 1826  # substract 5 years
+ds.to_netcdf('adamselv_forcing_feb-jun_evap0.nc')
